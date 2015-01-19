92cd914802d4b990b6c1c5007c04f1ef7d2dda48
Fabian Uribe <uribe@yahoo-inc.com>
2015-01-19 14:05:13 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Fabián Uribe Herrera on 1/19/15.
+//  Copyright (c) 2015 fabian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
